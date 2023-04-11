0458c7bcf5c35f105b3b154c2b1f57c48d59744a
Senator kay <senatorkay2@gmail.com>
2023-04-11 22:36:04 +0100

    Last commit

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,49 +3,59 @@ import "./LoginScreen.css";
 import Netflix from "../avatarimage/netflix.png";
 import SignupScreen from "./SignupScreen";
 
-
 const LoginScreen = () => {
   const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
         <img className="login_logo" src={Netflix} alt="" />
-        <button
-          className="loginScreen_button"
-          onClick={() => {
-            setSignIn(true);
-          }}
-        >
-          Get started
-        </button>
+        {signIn ? (
+          <button
+            className="loginScreen_button"
+            onClick={() => {
+              setSignIn(false);
+            }}
+          >
+            Back
+          </button>
+        ) : (
+          <button
+            className="loginScreen_button"
+            onClick={() => {
+              setSignIn(true);
+            }}
+          >
+            Get started
+          </button>
+        )}
 
         <div className="loginScreen_gradient"></div>
       </div>
 
       <div className="loginScreen_body">
         {signIn ? (
-            <SignupScreen/>
+          <SignupScreen />
         ) : (
-            <>
-          <h1>Unlimited films, TV programmes and more.</h1>
-          <h2>Watch anywhere. Cancel at any time.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership
-          </h3>
-          <div className="loginScreen_input">
-            <form>
-              <input type="email" placeholder="Email Address" />
-              <button
-                onClick={() => setSignIn(true)}
-                className=" loginScreen_getStarted"
-              >
-                GET STARTED
-              </button>
-            </form>
-          </div>
-        </>
-        ) }
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className=" loginScreen_getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
